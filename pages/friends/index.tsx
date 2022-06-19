68a4174210ad878d150fe8ca719e00f8ad7c3816
kutta97 <yhjin04170@gmail.com>
2022-06-19 21:22:44 +0900

    Layout 컴포넌트 코드 중복 제거

--- pages/friends/index.tsx
+++ pages/friends/index.tsx
@@ -1,69 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import { Header } from "../../components/header/Header";
-import { Sidebar } from "../../components/sidebar/Sidebar";
 
 const Friends: React.FC = () => {
 
-  const [friends] = useState(['1', '2', '3', '4', '5'])
-  const [groups] = useState([
-    '컴공 3학년 올 A+모임',
-    '토익 800점 이상 스터디'
-  ])
-
   return (
     <FriendsFrameStyled>
-      <Header className="header" selected="FRIENDS" />
-      <div className="BodyWrap">
-
-        <div className="sidebar">
-          <Sidebar nickname="Nickname" profileImage="null" description="hello world! from seoul I wanna be an awsome student." friends={friends} groups={groups} />
-        </div>
-
-        <div className="contentsWrap">
-        </div>
-
-      </div>
     </FriendsFrameStyled>
   )
 }
 
 const FriendsFrameStyled = styled.div`
-	display: flex;
-  width: 100%;
-  height: 100%;
-  flex-direction: column;
-  position: absolute;
-
-  .header {
-    width: 100%;
-    min-height: 120px;
-  }
-
-  .BodyWrap {
-    display: flex;
-    flex-direction: row;
-    position: relative;
-    width: 1366px;
-    height: 100%;
-    margin: auto;
-    padding-left: 10%;
-    padding-right: 10%;
-
-    .sidebar {
-      min-width: 230px;
-      height: 100%;
-    }
-
-    .contentsWrap {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      margin-top: 40px;
-      margin-left: 40px;
-
-    }
-  }
 `;
 
 export default Friends
