967c827b3ddfb3dbbbcc345f69732122c03e9b52
kutta97 <yhjin04170@gmail.com>
2022-09-22 06:46:18 +0900

    apply Layout component to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
+import { Layout } from '../components/layouts/layout/Layout';
 import GlobalStyles from '../styles/globalStyles';
 import { theme } from '../styles/theme';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Component {...pageProps} />
+      <Layout>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
   );
 };
