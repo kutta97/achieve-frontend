b8666292eff826a8439c72eabb033899833d2dda
kutta97 <yhjin04170@gmail.com>
2022-09-22 07:13:46 +0900

    add Per-Page Layout feature with typescript in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,32 @@
+import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import { ReactElement, ReactNode } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { Layout } from '../components/layouts/layout/Layout';
 import GlobalStyles from '../styles/globalStyles';
 import { theme } from '../styles/theme';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
     <ThemeProvider theme={theme}>
       <Layout>
-        <GlobalStyles />
-        <Component {...pageProps} />
+        {getLayout(
+          <>
+            <GlobalStyles />
+            <Component {...pageProps} />
+          </>
+        )}
       </Layout>
     </ThemeProvider>
   );
