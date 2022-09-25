1104ce4c8839c17b69e5daae05c74f42e7aa4d91
kutta97 <yhjin04170@gmail.com>
2022-09-25 17:33:23 +0900

    add sidebarProfile field in SidebarStore

--- store/components/layouts/sidebarLayout/sidebarStore.ts
+++ store/components/layouts/sidebarLayout/sidebarStore.ts
@@ -1,7 +1,40 @@
-import { makeObservable } from 'mobx';
+import { ISidebarProfile } from '@vo/layouts';
+import { action, makeObservable, observable } from 'mobx';
 
 export class SidebarStore {
+  sidebarProfile: ISidebarProfile = {
+    name: 'test',
+    description: 'test',
+  };
+
   constructor() {
-    makeObservable(this, {});
+    makeObservable(this, {
+      sidebarProfile: observable,
+      loadSidebar: action,
+    });
+
+    this.loadSidebar();
+  }
+
+  loadSidebar() {
+    this.getSidebar().then(() =>
+      console.log('sidebarProfile', this.sidebarProfile)
+    );
+  }
+
+  async getSidebar() {
+    try {
+      this.sidebarProfile = {
+        name: 'nickname',
+        description:
+          'hello world! from seoul\n' + 'I wanna be an awsome student.',
+        goalCount: 2,
+        goalList: [],
+        badgeCount: 3,
+        badgeList: [],
+      };
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
