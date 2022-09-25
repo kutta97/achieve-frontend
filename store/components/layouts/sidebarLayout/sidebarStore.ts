6bf9d5500a2530a9abe4f6c9240fd010a30a29c7
kutta97 <yhjin04170@gmail.com>
2022-09-25 18:46:54 +0900

    refactor SidebarStore

--- store/components/layouts/sidebarLayout/sidebarStore.ts
+++ store/components/layouts/sidebarLayout/sidebarStore.ts
@@ -2,10 +2,7 @@ import { ISidebarProfile } from '@vo/layouts';
 import { action, makeObservable, observable } from 'mobx';
 
 export class SidebarStore {
-  sidebarProfile: ISidebarProfile = {
-    name: 'test',
-    description: 'test',
-  };
+  sidebarProfile: ISidebarProfile;
 
   constructor() {
     makeObservable(this, {
@@ -17,9 +14,7 @@ export class SidebarStore {
   }
 
   loadSidebar() {
-    this.getSidebar().then(() =>
-      console.log('sidebarProfile', this.sidebarProfile)
-    );
+    this.getSidebar().then();
   }
 
   async getSidebar() {
@@ -29,7 +24,7 @@ export class SidebarStore {
         description:
           'hello world! from seoul\n' + 'I wanna be an awsome student.',
         goalCount: 2,
-        goalList: [],
+        goalList: ['소프트웨어공학 B+ 이상', '운전면허시험 합격'],
         badgeCount: 3,
         badgeList: [],
       };
