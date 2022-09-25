7d31f6ae3b2ff5e21811c919244c1d83b7151e06
kutta97 <yhjin04170@gmail.com>
2022-09-25 18:46:35 +0900

    fix sidebarStore object to get with destructing assignment

--- components/layouts/sidebarLayout/sidebar/hooks/useSidebar.ts
+++ components/layouts/sidebarLayout/sidebar/hooks/useSidebar.ts
@@ -3,15 +3,13 @@ import { ISidebarProfile } from '@vo/layouts';
 import { useEffect, useState } from 'react';
 
 export const useSidebar = () => {
-  const sidebarStore = useStores();
+  const { sidebarStore } = useStores();
 
   const [profile, setProfile] = useState<ISidebarProfile>();
 
   useEffect(() => {
     sidebarStore.sidebarProfile && setProfile(sidebarStore.sidebarProfile);
-    console.log('sidebarStore', sidebarStore);
-    console.log('sidebarStore sidebarProfile', sidebarStore.sidebarProfile);
-  }, [sidebarStore]);
+  }, [sidebarStore.sidebarProfile]);
 
   return { profile };
 };
