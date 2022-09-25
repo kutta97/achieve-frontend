f9b628718cdb6edc498ee0c8bb492d92c50b1c94
kutta97 <yhjin04170@gmail.com>
2022-09-25 17:32:17 +0900

    add sidebarStore in useSidebar hook

--- components/layouts/sidebarLayout/sidebar/hooks/useSidebar.ts
+++ components/layouts/sidebarLayout/sidebar/hooks/useSidebar.ts
@@ -1,8 +1,17 @@
+import { useStores } from '@hooks/useStores';
 import { ISidebarProfile } from '@vo/layouts';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useSidebar = () => {
+  const sidebarStore = useStores();
+
   const [profile, setProfile] = useState<ISidebarProfile>();
+
+  useEffect(() => {
+    sidebarStore.sidebarProfile && setProfile(sidebarStore.sidebarProfile);
+    console.log('sidebarStore', sidebarStore);
+    console.log('sidebarStore sidebarProfile', sidebarStore.sidebarProfile);
+  }, [sidebarStore]);
 
   return { profile };
 };
