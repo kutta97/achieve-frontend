a4755f28f9f88a3e085835113fadedb47084aef5
kutta97 <yhjin04170@gmail.com>
2022-09-27 16:57:52 +0900

    add useGroups hook

---
+++ fragments/groups/hooks/useGroups.ts
@@ -0,0 +1,15 @@
+import { useStores } from '@hooks/useStores';
+import { IGroup } from '@vo/groups/IGroup';
+import { useEffect, useState } from 'react';
+
+export const useGroups = () => {
+  const { groupsStore } = useStores();
+
+  const [groupList, setGroupList] = useState<IGroup[]>();
+
+  useEffect(() => {
+    groupsStore.groupList && setGroupList(groupsStore.groupList);
+  }, [groupsStore.groupList]);
+
+  return { groupList };
+};
