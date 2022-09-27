aaa1e6a9c2aa1b2a4f2c54a31657c50f34e7c8a8
kutta97 <yhjin04170@gmail.com>
2022-09-27 16:57:36 +0900

    add GroupsStore

---
+++ vo/groups/IGroup.ts
@@ -0,0 +1,18 @@
+export interface IGroup {
+  id?: number;
+  title?: string;
+  members?: IGroupMember[];
+  goals?: IGroupGoal[];
+}
+
+export interface IGroupMember {
+  id?: number;
+  name?: string;
+  imgSrc?: string;
+}
+
+export interface IGroupGoal {
+  title?: string;
+  period?: string;
+  dDay?: string;
+}
