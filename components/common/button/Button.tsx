e8647bc777606bfd1198c692f583b891f9c99b21
kutta97 <yhjin04170@gmail.com>
2022-09-27 14:33:18 +0900

    Add Button component

---
+++ components/common/button/Button.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  text?: string;
+}
+
+export const Button = ({ text }: Props) => {
+  return <ButtonStyled>{text}</ButtonStyled>;
+};
+
+const ButtonStyled = styled.button<{ width?: number; height?: number }>`
+  display: flex;
+  background: ${({ theme }) => theme.colors.SolidPurple};
+  border-radius: 8px;
+  padding: 8px 12px;
+  cursor: pointer;
+
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 16px;
+  color: ${({ theme }) => theme.colors.BasicWhite};
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.ShadeGray30};
+    background: ${({ theme }) => theme.colors.DarkPurple};
+  }
+`;
