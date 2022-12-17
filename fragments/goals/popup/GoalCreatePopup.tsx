d3ebebdaae06c7316bae147ceb722034a6eba2d5
kutta97 <yhjin04170@gmail.com>
2022-12-17 20:30:13 +0900

    GoalCreatePopup Title pattern 제거

--- fragments/goals/popup/GoalCreatePopup.tsx
+++ fragments/goals/popup/GoalCreatePopup.tsx
@@ -3,7 +3,7 @@ import { TextInput } from '@components/common/form/TextInput';
 import { DateInput } from '@components/common/form/goalInput/DateInput';
 import { ScoreInput } from '@components/common/form/goalInput/ScoreInput';
 import { Modal } from '@components/common/modal/Modal';
-import { NAME_PATTERN } from '@consts/Regex';
+import { observer } from 'mobx-react';
 import styled from 'styled-components';
 
 import { useGoalCreatePopup } from './hooks/useGoalCreatePopup';
@@ -13,64 +13,62 @@ interface Props {
   onClose?: () => void;
 }
 
-export const GoalCreatePopup = ({ isOpen = false, onClose }: Props) => {
-  const {
-    handleScoreTypeChange,
-    handleCreateClick,
-    startDate,
-    endDate,
-    changeStartDate,
-    changeEndDate,
-    control,
-  } = useGoalCreatePopup();
+export const GoalCreatePopup = observer(
+  ({ isOpen = false, onClose }: Props) => {
+    const {
+      handleScoreTypeChange,
+      handleCreateClick,
+      startDate,
+      endDate,
+      changeStartDate,
+      changeEndDate,
+      control,
+    } = useGoalCreatePopup();
 
-  return (
-    <Modal
-      title="Create New Goal"
-      width={638}
-      isOpen={isOpen}
-      onClose={onClose}
-    >
-      <GoalCreatePopupStyled>
-        <TextInput
-          label="Exam name"
-          name="examTitle"
-          rules={{
-            maxLength: {
-              value: 30,
-              message: '제목은 30자 이하로 입력해 주세요.',
-            },
-            pattern: {
-              value: NAME_PATTERN,
-              message: '이 항목은 한글, 영문만 입력이 가능합니다.',
-            },
-            required: '이 항목은 필수 입력값입니다.',
-          }}
-          placeholder="write your exam name"
-          control={control}
-        />
-        <ScoreInput
-          onChangeScoreType={handleScoreTypeChange}
-          control={control}
-        />
-        <span className="label-input-wrap">
-          <Label>Due date</Label>
-          <DateInput
-            startDate={startDate}
-            endDate={endDate}
-            onChangeStartDate={changeStartDate}
-            onChangeEndDate={changeEndDate}
+    return (
+      <Modal
+        title="Create New Goal"
+        width={638}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <GoalCreatePopupStyled>
+          <TextInput
+            label="Exam name"
+            name="examTitle"
+            rules={{
+              maxLength: {
+                value: 30,
+                message: '제목은 30자 이하로 입력해 주세요.',
+              },
+              required: '이 항목은 필수 입력값입니다.',
+            }}
+            placeholder="write your exam name"
+            control={control}
           />
-        </span>
-        <Button
-          text="Create"
-          className="submit-button"
-          onClick={handleCreateClick}
-        />
-      </GoalCreatePopupStyled>
-    </Modal>
-  );
-};
+          <ScoreInput
+            onChangeScoreType={handleScoreTypeChange}
+            control={control}
+          />
+          <span className="label-input-wrap">
+            <Label>Due date</Label>
+            <DateInput
+              startDate={startDate}
+              endDate={endDate}
+              onChangeStartDate={changeStartDate}
+              onChangeEndDate={changeEndDate}
+            />
+          </span>
+          <Button
+            text="Create"
+            className="submit-button"
+            onClick={handleCreateClick}
+          />
+        </GoalCreatePopupStyled>
+      </Modal>
+    );
+  }
+);
 
 export const GoalCreatePopupStyled = styled.form`
   display: flex;
